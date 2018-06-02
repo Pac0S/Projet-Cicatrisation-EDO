667c7a2f29fdc5a52580df604acbbc7b25c389e1
paco salomon <paco.salomon@insa-lyon.fr>
2018-06-02 20:02:18 +0200

    un petit début

--- cicatrisation.m
+++ cicatrisation.m
@@ -30,25 +30,25 @@ newc = zeros(J2,1);
 %Definition de la zone blessée
 for i = 8/h:12/h
     n((8+i*20)/h+i:(12+i*20)/h+i)=0;
-    c((8+i*20)/h:(12+i*20)/h)=0;
+    c((8+i*20)/h+i:(12+i*20)/h+i)=0;
 end
 
 
 
 %%paramètres de simulation
-tfinal = 100;
+tfinal = 200;
 t0 = 0;
 t = t0;
 dt = 0.1;
 
 
 %%Variables
-D = 1;
-Dc = 1;
+D = 0.01;
+Dc = 0.02;
 lambda = 1;
 cm = 2;
 H = 10;
-k = 0.01;
+k = 0.1;
 n0 = 1;
 c0 = 0.5;
 alpha = 0.1;
@@ -71,6 +71,23 @@ bord = [coinhautgauche, coinhautdroit, coinbasgauche, coinbasdroit, ...
     bordgauche, bordhaut, bordbas, borddroit];
 interieur = setdiff(1:J2, bord);
 
+% interieur
+L = L + sparse(interieur,interieur+1,1,J2,J2);
+L = L + sparse(interieur,interieur-1,1,J2,J2);
+L = L + sparse(interieur,interieur+J,1,J2,J2);
+L = L + sparse(interieur,interieur-J,1,J2,J2);
+
+% bords
+L = L + sparse(bordhaut,bordhaut+1,1,J2,J2);
+L = L + sparse(bordhaut,bordhaut+J-1,1,J2,J2);
+L = L + sparse(bordhaut,bordhaut+J,1,J2,J2);
+L = L + sparse(bordhaut,bordhaut-J,1,J2,J2);
+
+
+L = L + sparse(bordgauche,bordgauche+1,1,J2,J2);
+L = L + sparse(bordgauche,bordgauche-1,1,J2,J2);
+L = L + sparse(bordgauche,bordgauche+J,1,J2,J2);
+L = L + sparse(bordgauche,bordgauche+J*(J-1),1,J2,J2);
 % interieur
 L = L + sparse(interieur,interieur+1,1,J2,J2);
 L = L + sparse(interieur,interieur-1,1,J2,J2);
@@ -129,19 +146,28 @@ surf(X,Y,reshape(n,J,J),'EdgeColor','none');
 view(2)
 drawnow;
 tk = 0;
+pause
 
 % BOUCLE PRINCIPALE
 while t < tfinal
     drawnow;
-    a =  k*((2*cm*(h-beta)*c(i)));
-    b = (cm*cm)+c(i)^2;
-    Ln = del2(n);
-    Lc = del2(c);
-    newn = n + D*Ln + k*((2*cm*(h-beta)*c)/(cm*cm+sum(c.*c))+beta) - k*n  ;
-    newc = c + Dc*Lc + lambda*c0*(n/n0)*((n0^2+alpha^2)/(sum(n.*n)+alpha^2)) - lambda * c;
-    %newn = n + n*D*dt/(h^2)*L;
-    %newn = n*D*dt/(h^2)*L + k*((2*cm*(h-beta)*c)/(cm*cm)+c^2) - k*n;
-    %newc = c*Dc*dt/(h^2)*L + lambda*c0*n/n0*((n0^2+alpha^2)/(n.*n+alpha^2)) - lambda * c;
+    %a =  k*((2*cm*(h-beta)*c(i)));
+    %b = (cm*cm)+c(i)^2;
+    
+    %Ln = del2(n);
+    %Lc = del2(c);
+    %newn = n + D*Ln %+ k*((2*cm*(h-beta)*c)/(cm*cm+sum(c.*c))+beta) - k*n  ;
+    %newc = c + Dc*Lc %+ lambda*c0*(n/n0)*((n0^2+alpha^2)/(sum(n.*n)+alpha^2)) - lambda * c;
+    %newn = n + D*dt/(h^2)*L*n;
+    
+    newn = n + D*dt/(h^2)*L*n;% +k*c;%+ ...
+       %k*((2*cm*(h-beta)*c)/(cm*cm)+c.^2) - ...
+       %k*n;
+    
+    newc = c + Dc*dt/(h^2)*L*c;% + k*n;%+ ...
+       %lambda*c0*n/n0*((n0^2+alpha^2)/(n.^2+alpha^2)) - ...
+       %lambda * c;
+        
     n = newn;
     c = newc;
             
@@ -152,7 +178,11 @@ while t < tfinal
         view(2)
         drawnow;
         tk = 0;
-    end 
-    t = t + dt;
-    tk = tk + dt;
+    end
+    
+    t = t + k;
+    tk = tk + k;
+    
+    
 end
+
